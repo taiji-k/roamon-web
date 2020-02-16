3aa616c09a232665afb15ea033cbb671de50fadd
TakayukiTomatsuri <TakayukiTomatsuri@users.noreply.github.com>
2020-02-16 16:35:16 +0900

    Add page to create new contact info.

--- pages/create-contact-info.vue
+++ pages/create-contact-info.vue
@@ -1,6 +1,61 @@
 <template>
   <div class="container">
-    <h1>boke</h1>
+    <v-form v-model="valid">
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <v-text-field
+              v-model="contact_type"
+              :rules="nameRules"
+              :counter="10"
+              label="Contact_type"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <v-text-field
+              v-model="contact_dest"
+              :rules="nameRules"
+              :counter="10"
+              label="Contact_dest"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <v-text-field
+              v-model="prefix_list"
+              :rules="emailRules"
+              label="Prefix_list"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <v-text-field
+              v-model="asn_list"
+              :rules="emailRules"
+              label="ASN_list"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-btn class="mr-4" color="success" @click="submit">submit</v-btn>
+        </v-row>
+      </v-container>
+    </v-form>
   </div>
 </template>
 
@@ -10,15 +65,45 @@
 
   export default {
     name: "add-contact-info",
+    data: () => ({
+      valid: false,
+      contact_type: '',
+      contact_dest: '',
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => v.length <= 256 || 'Name must be less than 10 characters',
+      ],
+      prefix_list: '',
+      emailRules: [
+        v => !!v || 'E-mail is required'
+        // v => /.+@.+/.test(v) || 'E-mail must be valid',
+      ],
+      asn_list: '',
+    }),
+    methods: {
+      submit () {
+        console.log("buttoned!")
+        console.log(this.prefix_list.split(' '))
+        console.log(this.asn_list.split(' '))
+        //this.$v.$touch();
+
+        // DBに送信
+        axios
+          .post('/api/add-contact-info', {
+            contact_type: this.contact_type,
+            contact_dest: this.contact_dest,
+            prefix_list: this.prefix_list.replace(' ', '').split(','),
+            asn_list: this.asn_list.replace(' ', '').split(',')
+          })
+          .then(response => {
+            // this.items = response
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      },
+    },
     mounted: function () {
-      axios
-        .post('/api/add-contact-info',{ contact_type:"email", contact_dest:"example-fromweb@example.com", prefix_list:["192.168.40.0/24", "192.168.60.0/24"] , asn_list:[2020]})
-        .then(response => {
-          // this.items = response
-        })
-        .catch(error => {
-          console.log(error)
-        })
     }
   }
 </script>
