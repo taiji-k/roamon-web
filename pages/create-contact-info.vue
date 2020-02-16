10b265fedfd9cbacd8d7248106863b3f348d9546
TakayukiTomatsuri <TakayukiTomatsuri@users.noreply.github.com>
2020-02-16 16:04:27 +0900

    Create add-contact-info API

---
+++ pages/create-contact-info.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h1>boke</h1>
+  </div>
+</template>
+
+<script>
+  const querystring = require('querystring');
+  const axios = require('axios');
+
+  export default {
+    name: "add-contact-info",
+    mounted: function () {
+      axios
+        .post('/api/add-contact-info',{ contact_type:"email", contact_dest:"example-fromweb@example.com", prefix_list:["192.168.40.0/24", "192.168.60.0/24"] , asn_list:[2020]})
+        .then(response => {
+          // this.items = response
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- server/api.js
+++ server/api.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const bodyParser = require('body-parser');
+
+// POSTのbodyをパーするのに必要
+router.use(bodyParser.urlencoded({
+  extended: true
+}));
+router.use(bodyParser.json());
 
 const {Client} = require('pg');
 const client = new Client({
@@ -44,4 +51,63 @@ router.get('/all-rov-result', (req, res, next) => {
   })
 })
 
+router.post('/add-contact-info', (req, res, next) => {
+  console.log(req.body);
+  console.log(req.body.contact_type);
+  const query_insert_contact_info = {
+    text: 'INSERT INTO contact_informations VALUES (DEFAULT, $1, $2) ON CONFLICT DO NOTHING;',
+    values: [req.body.contact_type, req.body.contact_dest],
+  };
+  client.query(query_insert_contact_info, (err, dat) => {
+    if (err) {
+      console.log(err.stack)
+    } else {
+      const query_select_contact_info_id = {
+        text: 'SELECT id FROM contact_informations WHERE contact_type = $1 AND contact_information = $2;',
+        values: [req.body.contact_type, req.body.contact_dest],
+      };
+      client.query(query_select_contact_info_id, (err, dat) => {
+        if (err) {
+          console.log(err.stack)
+        } else {
+          const contact_info_id = dat.rows[0].id;
+          console.log(contact_info_id)
+          console.log(req.body.prefix_list)
+          console.log(req.body.prefix_list[0])
+
+
+          for (var i = 0; i < req.body.prefix_list.length; i++) {
+            console.log(req.body.prefix_list[i])
+            const query_insert_watched_prefix = {
+              text: 'INSERT INTO watched_prefixes  VALUES ($1, $2) ON CONFLICT DO NOTHING;',
+              values: [contact_info_id, req.body.prefix_list[i]],
+            };
+            client.query(query_insert_watched_prefix, (err, dat) => {
+              if (err) {
+                console.log(err.stack)
+              } else {
+
+              }
+            })
+          }
+
+          for (var i = 0; i < req.body.asn_list.length; i++) {
+            const query_insert_watched_asn = {
+              text: 'INSERT INTO watched_asns  VALUES ($1, $2) ON CONFLICT DO NOTHING;',
+              values: [contact_info_id, req.body.asn_list[i]],
+            };
+            client.query(query_insert_watched_asn, (err, dat) => {
+              if (err) {
+                console.log(err.stack)
+              } else {
+
+              }
+            })
+          }
+        }
+      })
+    }
+  })
+})
 module.exports = router
+
